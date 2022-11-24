d937137e3d8d4f5f73f23c99e4d44158cbccc0e9
Freddy Ayala <freddyayala@microsoft.com>
2022-11-24 10:14:01 +0100

    First Commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Declare the Terraform Module for Cloud Adoption Framework
+# Enterprise-scale and provide a base configuration.
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "~> 2.4.1"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.management   = azurerm.management
+    azurerm.connectivity = azurerm.connectivity
+  }
+
+  # Base module configuration settings.
+  root_parent_id   = data.azurerm_client_config.current.tenant_id
+  root_id          = local.root_id
+  root_name        = local.root_name
+  default_location = local.default_location
+
+  # Control deployment of the core landing zone hierachy.
+  deploy_core_landing_zones   = true
+  deploy_corp_landing_zones   = local.deploy_corp_landing_zones
+  deploy_online_landing_zones = local.deploy_online_landing_zones
+  deploy_sap_landing_zones    = local.deploy_sap_landing_zones
+  deploy_connectivity_resources    = local.deploy_connectivity_resources
+configure_connectivity_resources = local.configure_connectivity_resources
+
+  subscription_id_connectivity  = data.azurerm_client_config.current.subscription_id
+  deploy_management_resources = true
+  subscription_id_management = data.azurerm_client_config.current.subscription_id
+
+}
---
+++ variables.tf
@@ -0,0 +1,89 @@
+# The following variable are used to simplify the process of customizing 
+# important settings and provide the foundation for what needs to be true when 
+# deploying multiple instances of the module into a single Tenant.
+
+# With the exception of subscription_id_management and 
+# subscription_id_connectivity, these are all given values via the included 
+# .tfvars files.
+
+variable "subscription_id_management" {
+  type        = string
+  default     = ""
+  description = "Sets the Subscription ID to use for Management resources."
+}
+
+variable "subscription_id_connectivity" {
+  type        = string
+  default     = ""
+  description = "Sets the Subscription ID to use for Connectivity resources."
+}
+
+
+
+variable "default_location" {
+  type        = string
+  default     = "eastus"
+  description = "Sets the default location for resources, including references to location within Policy templates."
+}
+
+variable "deploy_corp_landing_zones" {
+  type        = bool
+  default     = false
+  description = "If set to true, will deploy the \"Corp\" landing zones in addition to any core and custom landing zones."
+}
+
+variable "deploy_online_landing_zones" {
+  type        = bool
+  default     = false
+  description = "If set to true, will deploy the \"Online\" landing zones in addition to any core and custom landing zones."
+}
+
+variable "deploy_sap_landing_zones" {
+  type        = bool
+  default     = false
+  description = "If set to true, will deploy the \"SAP\" landing zones in addition to any core and custom landing zones."
+}
+
+variable "deploy_management_resources" {
+  type        = bool
+  default     = false
+  description = "If set to true, will deploy the management resources in the Subscription assigned as the Management landing zone."
+}
+
+
+variable "security_contact_email_address" {
+  type        = string
+  default     = "security.contact@replace_me"
+  description = "Sets the security contact email address used when configuring Azure Security Center."
+}
+
+
+
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "myorg"
+}
+
+variable "root_name" {
+  type    = string
+  default = "My Organization"
+}
+
+variable "deploy_connectivity_resources" {
+  type    = bool
+  default = true
+}
+
+variable "connectivity_resources_location" {
+  type    = string
+  default = "uksouth"
+}
+
+variable "connectivity_resources_tags" {
+  type = map(string)
+  default = {
+    demo_type = "deploy_connectivity_resources_custom"
+  }
+}
